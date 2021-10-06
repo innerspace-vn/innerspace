f251ff93dbed66374a91c3369a3eefebfb846081
Phạm Quỳnh Trang <trangpq2310@gmail.com>
2021-10-07 00:40:22 +0700

    updtd

--- js/main.js
+++ js/main.js
@@ -239,3 +239,29 @@ function displayRandomImages()
       // display the new random image    
       document.body.appendChild(newImage); 
     }  
+
+
+// nav bar
+// Create the dropdown base
+$("<select />").appendTo("nav");
+
+// Create default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});
+
+//make it works pls
+$("nav select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
